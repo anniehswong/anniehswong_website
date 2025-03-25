89f0e66cf0f27f19fd7e441d095183e9fa9c5a7f
ahsw817 <ahsw817@gmail.com>
2025-03-25 12:26:01 -0700

    first commit

---
+++ main.js
@@ -0,0 +1,9 @@
+document.addEventListener("DOMContentLoaded", function() {
+    var typed = new Typed(".auto-type", {
+        strings: ["web developer", "dog lover", "noodle explorer"],
+        typeSpeed: 130,
+        backSpeed: 130,
+        loop: true,
+    });
+});
+
